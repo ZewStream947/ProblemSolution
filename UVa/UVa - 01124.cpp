7094f371bcfe0122db7decf145de700de9d4dae4
ZewStream <61370205+ZewStream947@users.noreply.github.com>
2021-05-20 14:48:44 +0700

    Add files via upload

---
+++ UVa/UVa - 01124.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL)->sync_with_stdio(false);
+    string str;
+    while (getline(cin, str))
+        cout << str << '\n';
+
+    return 0;
+}
